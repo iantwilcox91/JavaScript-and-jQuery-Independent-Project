3f3c2c98bd4a05ac2aba597e5379861dd76679cf
Ian Wilcox <ian@>
2016-08-12 15:42:23 -0700

    fix js indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -5,121 +5,105 @@ var rr = 0
 
 
 $(document).ready(function(){
- $(".start").click(function(){
-   $(".question1").show();
-   $(".start").hide();
-   $(".next1").show();
- });
+  $(".start").click(function(){
+    $(".question1").show();
+    $(".start").hide();
+    $(".next1").show();
+  });
 
- $(".next1").click(function(){
-   if ($("#Q1").val() === "C"){
-     cc = cc + 1
-   }else if ($("#Q1").val() === "J"){
-     jj = jj + 1
-   }else if ($("#Q1").val() === "P"){
-     pp = pp + 1
-   }else if ($("#Q1").val() === "R"){
-     rr = rr + 1
-   }else{
-   }
-   $(".question1").hide();
-   $(".question2").show();
+  $(".next1").click(function(){
+    if ($("#Q1").val() === "C"){
+    cc = cc + 1
+    }else if ($("#Q1").val() === "J"){
+    jj = jj + 1
+    }else if ($("#Q1").val() === "P"){
+    pp = pp + 1
+    }else if ($("#Q1").val() === "R"){
+    rr = rr + 1
+    }else{
+    }
+    $(".question1").hide();
+    $(".question2").show();
+    $(".next1").hide();
+    $(".next2").show();
+  });
 
-      $(".next1").hide();
-      $(".next2").show();
- });
+  $(".next2").click(function(){
+    if ($("#Q2").val() === "C"){
+      cc = cc + 1
+    }else if ($("#Q2").val() === "J"){
+      jj = jj + 1
+    }else if ($("#Q2").val() === "P"){
+      pp = pp + 1
+    }else if ($("#Q2").val() === "R"){
+      rr = rr + 1
+    }else{
+    }
+    $(".question2").hide();
+    $(".question3").show();
+    $(".next2").hide();
+    $(".next3").show();
+  });
 
- $(".next2").click(function(){
-   if ($("#Q2").val() === "C"){
-     cc = cc + 1
-   }else if ($("#Q2").val() === "J"){
-     jj = jj + 1
-   }else if ($("#Q2").val() === "P"){
-     pp = pp + 1
-   }else if ($("#Q2").val() === "R"){
-     rr = rr + 1
-   }else{
-   }
-   $(".question2").hide();
-   $(".question3").show();
+  $(".next3").click(function(){
+    if ($("#Q3").val() === "C"){
+      cc = cc + 1
+    }else if ($("#Q3").val() === "J"){
+      jj = jj + 1
+    }else if ($("#Q3").val() === "P"){
+      pp = pp + 1
+    }else if ($("#Q3").val() === "R"){
+      rr = rr + 1
+    }else{
+    }
+    $(".question3").hide();
+    $(".question4").show();
+    $(".next3").hide();
+    $(".next4").show();
+  });
 
-      $(".next2").hide();
-      $(".next3").show();
- });
+  $(".next4").click(function(){
+    if ($("#Q4").val() === "C"){
+      cc = cc + 1
+    }else if ($("#Q4").val() === "J"){
+      jj = jj + 1
+    }else if ($("#Q4").val() === "P"){
+      pp = pp + 1
+    }else if ($("#Q4").val() === "R"){
+      rr = rr + 1
+    }else{
+    }
+    $(".question4").hide();
+    $(".question5").show();
+    $(".next4").hide();
+    $(".next5").show();
+  });
 
- $(".next3").click(function(){
-   if ($("#Q3").val() === "C"){
-     cc = cc + 1
-   }else if ($("#Q3").val() === "J"){
-     jj = jj + 1
-   }else if ($("#Q3").val() === "P"){
-     pp = pp + 1
-   }else if ($("#Q3").val() === "R"){
-     rr = rr + 1
-   }else{
-   }
-   $(".question3").hide();
-   $(".question4").show();
-
-      $(".next3").hide();
-      $(".next4").show();
- });
-
- $(".next4").click(function(){
-   if ($("#Q4").val() === "C"){
-     cc = cc + 1
-   }else if ($("#Q4").val() === "J"){
-     jj = jj + 1
-   }else if ($("#Q4").val() === "P"){
-     pp = pp + 1
-   }else if ($("#Q4").val() === "R"){
-     rr = rr + 1
-   }else{
-   }
-   $(".question4").hide();
-   $(".question5").show();
-
-      $(".next4").hide();
-      $(".next5").show();
- });
-
- $(".next5").click(function(){
-   if ($("#Q5").val() === "C"){
-     cc = cc - 1
-   }else if ($("#Q5").val() === "J"){
-     jj = jj - 1
-   }else if ($("#Q5").val() === "P"){
-     pp = pp - 1
-   }else if ($("#Q5").val() === "R"){
-     rr = rr - 1
-   }else{
-   }
-   $(".question5").hide();
-   $(".resultText").show();
-   $(".EndNote").show();
-// BIG ol if statement deciding what result to show
-  if ( cc > jj && cc > pp && cc > rr ){
-  $(".resultC").show();
-}else if ( jj > cc && jj > pp && jj > rr ){
-  $(".resultJava").show();
-}else if ( pp > jj && pp > cc && pp > rr ){
-  $(".resultPhp").show();
-}else if ( rr > jj && rr > pp && rr > cc ){
-  $(".resultRuby").show();
-  }
-      $(".next5").hide();
-      $(".REstart").show();
- });
-
-
-
-
-
-
-
-
-
-
-
+  $(".next5").click(function(){
+    if ($("#Q5").val() === "C"){
+    cc = cc - 1
+    }else if ($("#Q5").val() === "J"){
+    jj = jj - 1
+    }else if ($("#Q5").val() === "P"){
+    pp = pp - 1
+    }else if ($("#Q5").val() === "R"){
+    rr = rr - 1
+    }else{
+    }
+    $(".question5").hide();
+    $(".resultText").show();
+    $(".EndNote").show();
+    if ( cc > jj && cc > pp && cc > rr ){
+    $(".resultC").show();
+    }else if ( jj > cc && jj > pp && jj > rr ){
+    $(".resultJava").show();
+    }else if ( pp > jj && pp > cc && pp > rr ){
+    $(".resultPhp").show();
+    }else if ( rr > jj && rr > pp && rr > cc ){
+    $(".resultRuby").show();
+    }
+    $(".next5").hide();
+    $(".REstart").show();
+  });
 
 });
